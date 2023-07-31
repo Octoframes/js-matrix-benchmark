acd002a0ac848aa6b4b409d294285edacf7251f6
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-31 21:12:48 +0200

    create 20 matrix

--- src/MatrixBenchmark.jsx
+++ src/MatrixBenchmark.jsx
@@ -1,12 +1,26 @@
 function MatrixBenchmark() {
-  
   var start = new Date().valueOf();
 
+  //   console.log("MatrixBenchmark");
 
-//   console.log("MatrixBenchmark");
+  // Function to generate a single random matrix
+  function generateRandomMatrix(rows, cols, maxVal) {
+    const matrix = new Array(rows);
+    for (let i = 0; i < rows; i++) {
+      const row = new Int8Array(cols);
+      for (let j = 0; j < cols; j++) {
+        row[j] = Math.floor(Math.random() * maxVal);
+      }
+      matrix[i] = row;
+    }
+    return matrix;
+  }
 
-
-
+  // Generate 20 random matrices
+  const matrices = new Array(20);
+  for (let i = 0; i < matrices.length; i++) {
+    matrices[i] = generateRandomMatrix(360, 360, 128);
+  }
 
   let duration = new Date().valueOf() - start;
 
