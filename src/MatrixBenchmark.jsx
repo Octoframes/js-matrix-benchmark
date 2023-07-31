4df10103aa9230eaa37b667a450a89dcc6aff666
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-31 22:05:56 +0200

    use ct-slices

--- src/MatrixBenchmark.jsx
+++ src/MatrixBenchmark.jsx
@@ -1,47 +1,34 @@
 import { useState } from "react";
 import MatrixImage from "./MatrixImage";
-
+import data from "./ct_slice_730_upper_legs_continuous.json";
 function MatrixBenchmark() {
   const [mytracker, setMyTracker] = useState(0);
-
-  // Function to generate a single random matrix
-  function generateRandomMatrix(rows, cols) {
-    const matrix = new Array(rows);
-    for (let i = 0; i < rows; i++) {
-      const row = new Float32Array(cols);
-      for (let j = 0; j < cols; j++) {
-        row[j] = Math.random();
-      }
-      matrix[i] = row;
-    }
-    return matrix;
-  }
-
-  // Generate 20 random matrices
-  const matrices = new Array(20);
-  for (let i = 0; i < matrices.length; i++) {
-    matrices[i] = generateRandomMatrix(360, 360);
-  }
+  const mydata = data["imgs"];
+  
+  // Create matrices from data
+  const matrices_from_data = Object.values(mydata).map((matrix) => {
+    return matrix.map((row) => {
+      return Float32Array.from(row);
+    });
+  });
 
   var start = new Date().valueOf();
 
   // Create an empty matrix to store the result
-  const resultMatrix = new Array(360).fill(0).map(() => new Float32Array(360).fill(0));
+  const resultMatrix = new Array(360)
+    .fill(0)
+    .map(() => new Float32Array(360).fill(0));
 
   // Add each matrix to the result
-  for (let matrix of matrices) {
+  for (let matrix of matrices_from_data) {
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < matrix[i].length; j++) {
-        resultMatrix[i][j] += matrix[i][j]*10;
+        resultMatrix[i][j] -= Math.floor(matrix[i][j] * 20);
       }
     }
   }
   let duration = new Date().valueOf() - start;
-
-
-
-
-  console.log(resultMatrix)
+  console.log(resultMatrix);
 
   function handleClick() {
     setMyTracker(mytracker + 1);
