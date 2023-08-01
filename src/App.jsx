f7b93e36d82d23001dc99319677f098e1526f5ef
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-01 09:42:50 +0200

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,42 @@
-import React, { useState, useRef } from "react";
-import logo from "./scanner.png";
+import React, { useState, useRef, useEffect } from "react";
+import scanner from "./scanner.png";
 
 const App = () => {
   const [rotateDegree, setRotateDegree] = useState(0);
-  const imgRef = useRef(null);
+
+  const imageRef = useRef(null);
 
   const handleMouseDown = (e) => {
-    const rect = imgRef.current.getBoundingClientRect();
+    const rect = imageRef.current.getBoundingClientRect();
     const x = rect.left + rect.width / 2;
     const y = rect.top + rect.height / 2;
     const rad = Math.atan2(e.clientY - y, e.clientX - x);
-    const deg = rad * (180 / Math.PI) +180;
+    const deg = rad * (180 / Math.PI) + 180;
 
     setRotateDegree(deg);
   };
 
   console.log(rotateDegree);
   const handleMouseMove = (e) => {
-    if (e.buttons === 1) { // if mouse button is pressed
+    if (e.buttons === 1) {
+      // if mouse button is pressed
       handleMouseDown(e);
     }
   };
 
   return (
-    <img
-      ref={imgRef}
-      src={logo}
-      alt="scanner"
-      style={{ transform: `rotate(${rotateDegree}deg)` }}
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      draggable={false}
-    />
+    <div className="background-left">
+      <img
+        id="centered-image"
+        ref={imageRef}
+        src={scanner}
+        alt="scanner"
+        style={{ transform: `rotate(${rotateDegree}deg)` }}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        draggable={false}
+      />
+    </div>
   );
 };
 
