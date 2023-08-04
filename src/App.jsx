eb2704e40956c2713615ff6b78a81f778a7b6b9d
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-04 13:04:15 +0200

    add new worker

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
 import React, { useEffect, useRef, useState } from 'react';
+// Assuming that worker.js is in public directory
 import MyWorker from './worker?worker'
 
-const worker = new MyWorker()
-
 const WorkerComponent = () => {
-    
-    worker.postMessage("hi");
-
+    const myWorker = new MyWorker()
+    const [inputValue, setInputValue] = useState('heyyy');
+    const [isWorkerBusy, setIsWorkerBusy] = useState(false);
+    const [latestValue, setLatestValue] = useState(null);
+    const [lastPostedValue, setLastPostedValue] = useState(null);
+    const [workerOutput, setWorkerOutput] = useState('');
+    // This function runs every time the inputValue state changes
+    useEffect(() => {
+        myWorker.postMessage(inputValue);
+        setLatestValue(inputValue);
+        console.log('Main start worker');
+        console.log(isWorkerBusy)
+        if (!isWorkerBusy) {
+          console.log('Main post message');
+          setLastPostedValue(inputValue);
+          setIsWorkerBusy(true);
+        }
+    }, [inputValue]);
+    useEffect(() => {
+        myWorker.onmessage = e => {
+            console.log('Message received from worker', e.data);
+            setWorkerOutput(e.data);
+            setIsWorkerBusy(false);
+        }
+    }, [])
+    //         // The worker has finished processing, it's not busy anymore
+    //         setIsWorkerBusy(false);
+    //         // If the latest value has changed since we last posted to the worker,
+    //         // post the latest value to the worker.
+    //         if (lastPostedValue !== latestValue) {
+                // setIsWorkerBusy(true);
+    //             setLastPostedValue(latestValue);
+    //             myWorker.postMessage(latestValue);
+    //         }
+    //     };
+    //     // Cleanup function to terminate worker when the component unmounts
+    //     return () => {
+    //         myWorker.terminate();
+    //     };
+    // }, [latestValue, lastPostedValue]);
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
     return (
         <div>
-            <h1>Worker Component</h1>
+            <input type="text" value={inputValue} onChange={handleInputChange} />
+            <h1>{workerOutput}</h1>
         </div>
     );
 };
-
 export default WorkerComponent;
--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,18 @@
 
 self.onmessage = function (e) {
-  console.log("Hello from worker.js");
-};
+  console.log("Message received from main script");
 
+  const startTime = new Date().getTime(); // Record the start time
+
+  for (let i = 0; i < 20e8; i++) {}
+
+  const endTime = new Date().getTime(); // Record the end time
+
+  console.log("Posting message back to main script");
+
+  const timeTaken = endTime - startTime;
+  const currentTime = `${timeTaken} ms`;
+
+  const workerResult = "Result: " + e.data + " " + currentTime;
+  postMessage(workerResult);
+};
