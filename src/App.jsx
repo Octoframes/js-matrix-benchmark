410ea18e528736aeb37cc48a331c61e592e771ec
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-04 12:42:09 +0200

    simplify

--- src/App.jsx
+++ src/App.jsx
@@ -19,35 +19,10 @@ const WorkerComponent = () => {
           console.log('Main post message');
           myWorker.postMessage(inputValue);
           setLastPostedValue(inputValue);
-          setIsWorkerBusy(true);
+        //   setIsWorkerBusy(true);
         }
     }, [inputValue]);
 
-    useEffect(() => {
-        myWorker.onmessage = e => {
-            console.log('Message received from worker', e.data);
-            setWorkerOutput(e.data);
-            setIsWorkerBusy(false);
-
-        }
-    }, [])
-    //         // The worker has finished processing, it's not busy anymore
-    //         setIsWorkerBusy(false);
-
-    //         // If the latest value has changed since we last posted to the worker,
-    //         // post the latest value to the worker.
-    //         if (lastPostedValue !== latestValue) {
-                // setIsWorkerBusy(true);
-    //             setLastPostedValue(latestValue);
-    //             myWorker.postMessage(latestValue);
-    //         }
-    //     };
-
-    //     // Cleanup function to terminate worker when the component unmounts
-    //     return () => {
-    //         myWorker.terminate();
-    //     };
-    // }, [latestValue, lastPostedValue]);
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
