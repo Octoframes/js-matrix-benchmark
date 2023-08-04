19ebabfadcc17ce60898fb0dec849f5f43057e03
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-04 11:59:23 +0200

    worker updates always

--- worker.js
+++ worker.js
@@ -1,27 +1,19 @@
-// function hello() {
-//     let name = "foo"
-//     for (let i = 0; i < 10e8; i++) {}
-//     let date = new Date();
-//     let currentTime = `${date.getSeconds()}.${date.getMilliseconds()} sec`;
-//     return `Val: ${name}!  Time: ${currentTime}`;
-//   }
 
 self.onmessage = function (e) {
   console.log("Message received from main script");
-  for (let i = 0; i < 50e8; i++) {}
+
+  const startTime = new Date().getTime(); // Record the start time
+
+  for (let i = 0; i < 10e8; i++) {}
+
+  const endTime = new Date().getTime(); // Record the end time
+
   console.log("Posting message back to main script");
-  let date = new Date();
-  let currentTime = `${date.getSeconds()}.${date.getMilliseconds()} sec`;
-  
-  var workerResult = "Result: " + e.data + currentTime;
+
+  const timeTaken = endTime - startTime;
+  const currentTime = `${timeTaken} ms`;
+
+  const workerResult = "Result: " + e.data + " " + currentTime;
   postMessage(workerResult);
 };
 
-
-
-// export function hello(name) {
-//   for (let i = 0; i < 10e8; i++) {}
-//   let date = new Date();
-//   let currentTime = `${date.getSeconds()}.${date.getMilliseconds()} sec`;
-//   return `Val: ${name}!  Time: ${currentTime}`;
-// }
