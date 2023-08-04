7e7aaa5eca138f3cf59ba8077c704309fbcdf4f3
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-04 13:44:05 +0200

    make worker artificial slow

--- src/worker.js
+++ src/worker.js
@@ -1,21 +1,19 @@
-
 self.onmessage = function (e) {
-
   // console.log("Message received " + e.data);
-
 
   const startTime = new Date().getTime(); // Record the start time
 
-  for (let i = 0; i < 20e8; i++) {}
+  let z = 0;
+  for (let i = 0; i < 4e8; i++) {
+    z += i;
+  }
+  console.log(z);
 
   const endTime = new Date().getTime(); // Record the end time
-
 
   const timeTaken = endTime - startTime;
   const currentTime = `${timeTaken} ms`;
 
   const workerResult = "Result: " + e.data + " " + currentTime;
-  console.log(workerResult);
   postMessage(workerResult);
-
 };
