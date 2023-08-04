84f47efa14e2e21bb9fc0d4824a9777756d0b67a
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-04 13:21:48 +0200

    update worker

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,8 @@
 
 self.onmessage = function (e) {
-  console.log("Message received from main script");
+
+  console.log("Message received " + e.data);
+
 
   const startTime = new Date().getTime(); // Record the start time
 
@@ -8,11 +10,12 @@ self.onmessage = function (e) {
 
   const endTime = new Date().getTime(); // Record the end time
 
-  console.log("Posting message back to main script");
 
   const timeTaken = endTime - startTime;
   const currentTime = `${timeTaken} ms`;
 
   const workerResult = "Result: " + e.data + " " + currentTime;
+  console.log(workerResult);
   postMessage(workerResult);
+
 };
