e702c8d860aa0911c3cc6071d1a74c5370584e19
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-04 12:30:03 +0200

    react worker

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,61 @@
-import React, { useEffect, useState } from "react";
-import { createWorkerFactory, useWorker } from "@shopify/react-web-worker";
+import React, { useEffect, useRef, useState } from 'react';
 
-const createWorker = createWorkerFactory(() => import("./worker"));
+// Assuming that worker.js is in public directory
+const myWorker = new Worker('./worker.js');
 
-function Home() {
+const WorkerComponent = () => {
+    const [inputValue, setInputValue] = useState('heyyy');
+    const [isWorkerBusy, setIsWorkerBusy] = useState(false);
+    const [latestValue, setLatestValue] = useState(null);
+    const [lastPostedValue, setLastPostedValue] = useState(null);
+    const [workerOutput, setWorkerOutput] = useState('');
 
-  const worker = useWorker(createWorker);
-  const [message, setMessage] = React.useState(null);
-  const [myValue, setMyValue] = useState("");
-  // const [isWorking, setIsWorking] = useState(false);
+    // This function runs every time the inputValue state changes
+    useEffect(() => {
+        setLatestValue(inputValue);
+        console.log('Main start worker');
+        console.log(isWorkerBusy)
+        if (!isWorkerBusy) {
+          console.log('Main post message');
+          myWorker.postMessage(inputValue);
+          setLastPostedValue(inputValue);
+          setIsWorkerBusy(true);
+        }
+    }, [inputValue]);
 
-  useEffect(() => {
-    (async () => {
-      // Note: in your actual app code, make sure to check if Home
-      // is still mounted before setting state asynchronously!
-      const webWorkerMessage = await worker.hello(myValue);
-      setMessage(webWorkerMessage);
-    })();
-  }, [worker]);
+    // useEffect(() => {
+    //     myWorker.onmessage = e => {
+    //         console.log('Message received from worker', e.data);
+    //         setWorkerOutput(e.data);
 
-  useEffect(() => {
-    console.log("myValue", myValue);
-  }, [myValue]);
+    //         // The worker has finished processing, it's not busy anymore
+    //         setIsWorkerBusy(false);
 
-  return (
-    <>
-      <input value={myValue} onChange={(e) => setMyValue(e.target.value)} />
-      <h1> {message} </h1>
-    </>
-  );
-}
+    //         // If the latest value has changed since we last posted to the worker,
+    //         // post the latest value to the worker.
+    //         if (lastPostedValue !== latestValue) {
+    //             setIsWorkerBusy(true);
+    //             setLastPostedValue(latestValue);
+    //             myWorker.postMessage(latestValue);
+    //         }
+    //     };
 
-export default Home;
+        // Cleanup function to terminate worker when the component unmounts
+    //     return () => {
+    //         myWorker.terminate();
+    //     };
+    // }, [latestValue, lastPostedValue]);
 
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
-// import {useState } from "react";
-// function slowFunction() {
-//   for (let i = 0; i < 5e8; i++) {}
-// }
-// function SlowComponent({ value }) {
-//   slowFunction();
-//   return <h1>Slow Component {value} </h1>;
-// }
-// export default function App() {
-//   const [myValue, setMyValue] = useState("");
-//   return (
-//     <>
-//       <label>
-//         My fast input:
-//         <input value={myValue} onChange={(e) => setMyValue(e.target.value)} />
-//       </label>
-//       <SlowComponent value={myValue} />
-//     </>
-//   );
-// }
+    return (
+        <div>
+            <input type="text" value={inputValue} onChange={handleInputChange} />
+            <h1>{workerOutput}</h1>
+        </div>
+    );
+};
+
+export default WorkerComponent;
