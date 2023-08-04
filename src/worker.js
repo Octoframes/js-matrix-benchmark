86bb18c1b4282748fbce307682fe1320afb38f1c
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-04 10:17:00 +0200

    simplify

--- src/worker.js
+++ src/worker.js
@@ -2,6 +2,6 @@ export function hello(name) {
     for (let i = 0; i < 5e8; i++) {}
     let date = new Date();
     let currentTime = `${date.getSeconds()}.${date.getMilliseconds()} seconds`;
-    return `Hello, ${name}! Time: ${currentTime}`;
+    return `Val: ${name}!  Time: ${currentTime}`;
   }
   
