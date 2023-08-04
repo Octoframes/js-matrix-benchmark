9525ebd15aae0f04cfebc75b5deec1c931d30852
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-04 12:52:43 +0200

    add worker

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
+import MyWorker from './worker?worker'
 
-// Assuming that worker.js is in public directory
-const myWorker = new Worker('./worker.js');
+const worker = new MyWorker()
 
 const WorkerComponent = () => {
-    const [inputValue, setInputValue] = useState('heyyy');
-    const [isWorkerBusy, setIsWorkerBusy] = useState(false);
-    const [latestValue, setLatestValue] = useState(null);
-    const [lastPostedValue, setLastPostedValue] = useState(null);
-    const [workerOutput, setWorkerOutput] = useState('');
-
-    // This function runs every time the inputValue state changes
-    useEffect(() => {
-        setLatestValue(inputValue);
-        console.log('Main start worker');
-        console.log(isWorkerBusy)
-        if (!isWorkerBusy) {
-          console.log('Main post message');
-          myWorker.postMessage(inputValue);
-          setLastPostedValue(inputValue);
-        //   setIsWorkerBusy(true);
-        }
-    }, [inputValue]);
-
-
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value);
-    };
+    
+    worker.postMessage("hi");
 
     return (
         <div>
-            <input type="text" value={inputValue} onChange={handleInputChange} />
-            <h1>{workerOutput}</h1>
+            <h1>Worker Component</h1>
         </div>
     );
 };
