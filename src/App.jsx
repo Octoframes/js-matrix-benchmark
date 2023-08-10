a87fcb795f4625cdaaacd65c62ef80f930da3c31
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-10 13:46:37 +0200

    try json

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import myjson from "./assets/TEST.json";
-const App = () => {
-  console.log(myjson);
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+
+function App() {
+  const [testData, setTestData] = useState(null);
+
+  useEffect(() => {
+    fetch("/TEST.json")
+      .then((response) => response.json())
+      .then((data) => setTestData(data))
+      .catch((error) => console.error("There was an error!", error));
+  }, []);
+
   return (
     <div>
-      <h1>Hello world</h1>
+      {testData && <pre>{JSON.stringify(testData, null, 2)}</pre>}
     </div>
   );
-};
+}
 
 export default App;
